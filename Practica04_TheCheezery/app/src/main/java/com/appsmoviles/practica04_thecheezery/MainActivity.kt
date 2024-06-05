845f24b4ed5fd8cde41c66b9758483307c604387
Jorge Estrada <jorgeaestrada45@gmail.com>
2024-06-04 23:09:29 -0700

    Practica04

---
+++ Practica04_TheCheezery/app/src/main/java/com/appsmoviles/practica04_thecheezery/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.appsmoviles.practica04_thecheezery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
